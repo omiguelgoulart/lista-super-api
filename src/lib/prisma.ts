edf2b95d9da4ae23d8b51c4dc20d814096ece23f
Luis Miguel Goulart <luismiguelgoulart@gmail.com>
2026-05-30 17:33:25 -0300

    🔧 chore: configura cliente Prisma singleton

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({
+  adapter,
+  log: ["query", "error", "warn"],
+});
+
+export default prisma;
