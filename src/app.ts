96b8bb9c99159257999330545403f90350d0a854
Luis Miguel Goulart <luismiguelgoulart@gmail.com>
2026-05-30 17:47:56 -0300

    🔧 chore: registra rotas de autenticação no app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { userRouter } from "./routes/userRoute";
+import { authRouter } from "./routes/auth";
 import { errorHandler } from "./middlewares/errorHandler";
 import { apiKeyMiddleware } from "./middlewares/apiKey";
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(apiKeyMiddleware);
 
+app.use("/auth", authRouter);
 app.use("/users", userRouter);
 
 app.use(errorHandler);
